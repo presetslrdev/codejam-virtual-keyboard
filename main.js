c7ceabb4e832fa3bfc6b036f1ebdd8c1f3f6ebb8
Denis <presetslr@gmail.com>
2019-11-06 00:50:02 +0300

    add sinchronization real and virtual keybord
    refactoring keys
    add sincronization
    add localStore
    add CapsLock state

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let lang = 'ru';
+let lang = localStorage.lang ? localStorage.lang : 'ru';
 let textBox = '';
+let capsLockState = 0;
 window.onload = function () {
 
     let textArea = document.createElement('textarea');
@@ -11,32 +12,88 @@ window.onload = function () {
         let ul = document.createElement('ul');
         ul.className = keyClassName(i);
         div.appendChild(ul);
-        keys[lang][i].forEach(el => {
+        for(let key in keys[lang][i]){
             let li = document.createElement('li');
-            li.innerHTML += '<a href="#" ' + (el.length > 2 ? ('class="' + el + '"') : ('')) + '>' + (!Array.isArray(el) ? ('<span>' + el + '</span>') : ('<span>' + el[0] + '</span>' + '<span>' + el[1] + '</span>')) + '</a>';
+            li.innerHTML += '<a href="#" ' + (keys[lang][i][key].length > 0 ? ('class="' + key.toLowerCase() + '"') : ('')) + '>' + (!Array.isArray(keys[lang][i][key]) ? ('<span>' + keys[lang][i][key] + '</span>') : ('<span>' + keys[lang][i][key][0] + '</span>' + '<span>' + keys[lang][i][key][1] + '</span>')) + '</a>';
             ul.appendChild(li);
-        });
+        }
     }
     document.body.append(div);
 
-    var list = document.querySelectorAll('a');
+    let list = document.querySelectorAll('a');
     for (let i = 0; list.length > i; i++) {
         list[i].onclick = function () {
+            let symbol = list[i].querySelector('span').textContent;
             list[i].classList.add('animation');
-            textBox += list[i].querySelector('span').textContent;
-            updateText(textBox);
+            if (symbol.length === 1) {
+                updateText(symbol);
+            } else {
+                functionalButton(symbol.toLowerCase());
+            }
             setTimeout(function () {
                 list[i].classList.remove('animation');
             }, 300)
         }
     }
     document.querySelector('.Lng').onclick = function () {
-        lang = lang === 'en' ? 'ru' : 'en';
+        lang = localStorage.lang = lang === 'en' ? 'ru' : 'en';
     };
+
+    document.querySelector('textarea').addEventListener('keyup', e => {
+        console.log('Caret at: ', e.target.selectionStart)
+    });
+
 };
 
+window.addEventListener("keydown", function (event) {
+    let keyCode = event.code.toLowerCase();
+    console.log(keyCode);
+    document.querySelector('.' + keyCode).classList.add("active");
+}, false);
+window.addEventListener("keyup", function (event) {
+    let keyCode = event.code.toLowerCase();
+    setTimeout(function () {
+        document.querySelector('.' + keyCode).classList.remove("active")
+    }, 150);
+}, false);
+
 function updateText(text) {
-    document.querySelector('textarea').value = text;
+    textBox += text;
+    document.querySelector('textarea').value = textBox;
+}
+
+function removeCharAt(index) {
+    let textInArea = document.querySelector('textarea').value;
+    let element = textInArea.split('');
+    element.splice((index || 0) - 1, 1);
+    textBox = element.join('');
+    document.querySelector('textarea').value = textBox;
+}
+
+function functionalButton(btn) {
+    switch (btn) {
+        case 'space':
+            updateText(' ');
+            break;
+        case 'enter':
+            updateText('\n');
+            break;
+        case 'tab':
+            updateText('\t');
+            break;
+        case 'backspace':
+            removeCharAt();
+            break;
+        case 'capslock':
+            capsLockState = capsLockState === 0 ? 1 : 0;
+            capsLockState ? document.querySelector('.capslock').classList.add("active") : document.querySelector('.capslock').classList.remove("active")
+            break;
+        case 'shift':
+        case 'alt':
+        case 'ctrl':
+        case 'win':
+            break;
+    }
 }
 
 function keyClassName(keyLine) {
@@ -59,88 +116,157 @@ function keyClassName(keyLine) {
 }
 
 let keys = {
-    en: [
-        [
-            ['`', '~'],
-            ['1', '!'],
-            ['2', '@'],
-            ['3', '#'],
-            ['4', '$'],
-            ['5', '%'],
-            ['6', '^'],
-            ['7', '&amp;'],
-            ['8', '*'],
-            ['9', '('],
-            ['0', ')'],
-            ['-', '_'],
-            ['=', '+'],
-            'backspace'
+        en: [
+            {
+                Backquote: ['`', '~'],
+                Digit1: ['1', '!'],
+                Digit2: ['2', '@'],
+                Digit3: ['3', '#'],
+                Digit4: ['4', '$'],
+                Digit5: ['5', '%'],
+                Digit6: ['6', '^'],
+                Digit7: ['7', '&amp;'],
+                Digit8: ['8', '*'],
+                Digit9: ['9', '('],
+                Digit0: ['0', ')'],
+                Minus: ['-', '_'],
+                Equal: ['=', '+'],
+                Backspace: 'Backspace'
+            },
+            {
+                Tab: 'Tab',
+                KeyQ: 'q',
+                KeyW: 'w',
+                KeyE: 'e',
+                KeyR: 'r',
+                KeyT: 't',
+                KeyY: 'y',
+                KeyU: 'u',
+                KeyI: 'i',
+                KeyO: 'o',
+                KeyP: 'p',
+                BracketLeft: ['[', '{'],
+                BracketRight: [']', '}'],
+                Lng: 'Lng'
+            },
+            {
+                CapsLock: 'CapsLock',
+                KeyA: 'a',
+                KeyS: 's',
+                KeyD: 'd',
+                KeyF: 'f',
+                KeyG: 'g',
+                KeyH: 'h',
+                KeyJ: 'j',
+                KeyK: 'k',
+                KeyL: 'l',
+                Semicolon: [';', ':'],
+                Quote: ["'", '&quot;'],
+                Backslash: ['\\', '|'],
+                Enter: 'Enter'
+            },
+            {
+                ShiftLeft: 'Shift',
+                KeyZ: 'z',
+                KeyX: 'x',
+                KeyC: 'c',
+                KeyV: 'v',
+                KeyB: 'b',
+                KeyN: 'n',
+                KeyM: 'm',
+                Comma: [',', '&lt;'],
+                Period: ['.', '&gt;'],
+                Slash: ['/', '?'],
+                ShiftRight: 'Shift'
+            },
+            {
+                ControlLeft: 'Ctrl',
+                MetaLeft: 'Win',
+                AltLeft: 'Alt',
+                Space: 'Space',
+                AltRight: 'Alt',
+                ControlRight: 'Ctrl',
+                ArrowLeft: 'Left',
+                ArrowUp: 'Up',
+                ArrowDown: 'Down',
+                ArrowRight: 'Right'
+            }
         ],
-        [
-            'tab',
-            'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-            ['[', '{'],
-            [']', '}'],
-            'Lng'
-        ],
-        [
-            'capslock',
-            'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-            [';', ':'],
-            ["'", '&quot;'],
-            ['\\', '|'],
-            'enter'
-        ],
-        [
-            'shift',
-            'z', 'x', 'c', 'v', 'b', 'n', 'm',
-            [',', '&lt;'],
-            ['.', '&gt;'],
-            ['/', '?'],
-            'shift'
-        ],
-        [
-            'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', 'Left', ['Up', 'Down'], 'Right'
+        ru: [
+            {
+                Backquote: 'ё',
+                Digit1: ['1', '!'],
+                Digit2: ['2', '"'],
+                Digit3: ['3', '№'],
+                Digit4: ['4', ';'],
+                Digit5: ['5', '%'],
+                Digit6: ['6', ':'],
+                Digit7: ['7', '?'],
+                Digit8: ['8', '*'],
+                Digit9: ['9', '('],
+                Digit0: ['0', ')'],
+                Minus: ['-', '_'],
+                Equal: ['=', '+'],
+                Backspace: 'Backspace'
+            },
+            {
+                Tab: 'Tab',
+                KeyQ: 'й',
+                KeyW: 'ц',
+                KeyE: 'у',
+                KeyR: 'к',
+                KeyT: 'е',
+                KeyY: 'н',
+                KeyU: 'г',
+                KeyI: 'ш',
+                KeyO: 'щ',
+                KeyP: 'з',
+                BracketLeft: 'х',
+                BracketRight: 'ъ',
+                Lng: 'Lng'
+            },
+            {
+                CapsLock: 'Capslock',
+                KeyA: 'ф',
+                KeyS: 'ы',
+                KeyD: 'в',
+                KeyF: 'а',
+                KeyG: 'п',
+                KeyH: 'р',
+                KeyJ: 'о',
+                KeyK: 'л',
+                KeyL: 'д',
+                Semicolon: 'ж',
+                Quote: 'э',
+                Backslash: ['\\', '/'],
+                Enter: 'Enter'
+            },
+            {
+                ShiftLeft: 'Shift',
+                KeyZ: 'я',
+                KeyX: 'ч',
+                KeyC: 'с',
+                KeyV: 'м',
+                KeyB: 'и',
+                KeyN: 'т',
+                KeyM: 'ь',
+                Comma: 'б',
+                Period: 'ю',
+                Slash: ['.', ','],
+                ShiftRight: 'Shift'
+            },
+            {
+                ControlLeft: 'Ctrl',
+                MetaLeft: 'Win',
+                AltLeft: 'Alt',
+                Space: 'Space',
+                AltRight: 'Alt',
+                ControlRight: 'Ctrl',
+                ArrowLeft: 'Left',
+                ArrowUp: 'Up',
+                ArrowDown: 'Down',
+                ArrowRight: 'Right'
+            }
         ]
-    ],
-    ru: [
-        [
-            'ё',
-            ['1', '!'],
-            ['2', '"'],
-            ['3', '№'],
-            ['4', ';'],
-            ['5', '%'],
-            ['6', ':'],
-            ['7', '?'],
-            ['8', '*'],
-            ['9', '('],
-            ['0', ')'],
-            ['-', '_'],
-            ['=', '+'],
-            'backspace'
-        ],
-        [
-            'tab',
-            'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ',
-            'Lng'
-        ],
-        [
-            'capslock',
-            'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э',
-            [';', ':'],
-            ["'", '&quot;'],
-            ['\\', '/'],
-            'enter'
-        ],
-        [
-            'shift',
-            'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю',
-            ['.', ','],
-            'shift'
-        ],
-        [
-            'Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', 'Left', ['Up', 'Down'], 'Right'
-        ]
-    ]
-};
+    }
+;
